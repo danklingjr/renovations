dd493ab28359e8e24a91c56e6e9e3636034b406d
danklingjr <danielklingjr@gmail.com>
2014-08-15 14:42:47 -0400

    sub page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,4 @@
 
+$('.mobile__button p').on({ 'touchstart' : function(){ 
+	$('.mobile-nav').find('.mobile__nav').slideToggle(200);
+}});
---
+++ js/min/main-min.js
@@ -0,0 +1,1 @@
+$(".mobile__button p").on({touchstart:function(){$(".mobile-nav").find(".mobile__nav").slideToggle(200)}});
